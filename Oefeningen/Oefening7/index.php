8eef3a7c2e394ebf20f55b30a95495cb67dbb527
Owain van Brakel <owain.vanbrakel@gmail.com>
2014-11-09 22:30:47 +0100

    Oefening 7

---
+++ Oefeningen/Oefening7/index.php
@@ -0,0 +1,23 @@
+<?php
+
+require 'Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+$app->get(
+    '/',
+    function () {
+        echo "Ja, hij doet het!";
+    }
+);
+
+$app->get(
+    '/test',
+    function () {
+        echo "Testing, 1, 2, 3!";
+    }
+);
+
+$app->run();
