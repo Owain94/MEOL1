eb174bd7e2ed314ce74667ce039b8564ffc0549f
Owain van Brakel <owain.vanbrakel@gmail.com>
2014-11-09 22:54:42 +0100

    Oefening 9

---
+++ Oefeningen/Oefening9/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+$app->get(
+    '/test3/:one/:two',
+    function($one, $two) {
+        echo "De eerste parameter is: " . $one . "<br />";
+        echo "De tweede parameter is: " . $two;
+    }
+);
+
+$app->run();
