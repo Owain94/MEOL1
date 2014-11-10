61afc545987c227fd4ebc5233a90fa4f57dc703d
Owain van Brakel <owain.vanbrakel@gmail.com>
2014-11-10 13:59:52 +0100

    Added Keys.php placeholder
    updated using git update-index --assume-unchanged

---
+++ Keys.php
@@ -0,0 +1,17 @@
+<?
+/**
+ * Created by PhpStorm.
+ * User: Owain
+ * Date: 09/11/14
+ * Time: 05:55
+ */
+
+define('APIKEY', 'WUNDERGROUND_API_KEY');
+
+define('DB_HOST', 'DATABASE_HOST');
+define('DB_USER', 'DATABASE_USER');
+define('DB_PASS', 'DATABASE_PASSWORD');
+define('DB_NAME', 'DATABASE_NAME');
+
+define('CONSUMER_KEY', 'CONSUMER_KEY');
+define('PRIVATE_KEY', 'CONSUMER_SECRET');
