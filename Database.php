75b1be6c3943ae929aa30563cf4c79d2e493c4aa
Owain van Brakel <owain.vanbrakel@gmail.com>
2014-11-14 09:35:08 +0100

    Correctly handle NULL values

--- Database.php
+++ Database.php
@@ -81,7 +81,7 @@ class Database
                     $type = PDO::PARAM_STR;
             }
         }
-        $this->stmt->bindValue($param, $value, $type);
+        $this->stmt->bindValue($param, $value == '' ? NULL : $value, $type);
     }
 
     /**
